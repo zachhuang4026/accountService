d7e32359e61a4e43f221861256ba991300b5ed08
adam-lim1 <adam.lim.1@gmail.com>
2022-11-16 20:54:56 -0600

    README w/ container setup, Python script to initialize DB

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 **Creating Docker Container for Account Service**
 ```bash
-# This process for creating Flask Container needs improved
-docker run -it -p 8080:8080 --name AccountService alpine
-apk add --update python3 py3-pip
-pip install flask
-
+docker run -p 5001:5001 --name AccountService -e POSTGRES_PASSWORD=secret_password -d postgres:15.1-alpine
 docker exec -it AccountService /bin/sh
+apk add postgresql-dev gcc python3-dev py3-pip musl-dev
+pip install flask
+pip install psycopg2 # https://github.com/psycopg/psycopg2/issues/684#issuecomment-392015532
+mkdir /accountService
+
+# Create Account DB [Postgres Shell]
+psql --username postgres
+create database accounts;
+exit
+
+# Run Python script to setup ACCOUNT_DIM table with default values
+cd /accountService
+python3 db_setup.py
 ```
+
+https://luppeng.wordpress.com/2020/02/28/install-and-start-postgresql-on-alpine-linux/
---
+++ db_setup.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+try:
+    # Create connection to database 
+    conn = psycopg2.connect("dbname='accounts' user='postgres' host='localhost' password='secret_password'")
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+    print('DB CONNECTION ESTABLISHED........')
+
+    # Define Account dim table
+    drop_sql = '''
+    DROP TABLE IF EXISTS ACCOUNT_DIM;
+    '''
+    cursor.execute(drop_sql)
+
+    create_sql = '''
+    CREATE TABLE IF NOT EXISTS ACCOUNT_DIM(
+        ACCOUNT_ID INT NOT NULL,
+        NAME TEXT NOT NULL,
+        EMAIL TEXT NOT NULL,
+        PASSWORD TEXT NOT NULL,
+        ACCOUNT_STATUS TEXT NOT NULL,
+        IS_ADMIN BOOLEAN NOT NULL
+    )
+    '''
+    cursor.execute(create_sql)
+    print("ACCOUNT_DIM Table created successfully........")
+
+    # INSERT DEFAULT VALUES
+    insert_sql = '''
+    INSERT INTO ACCOUNT_DIM (ACCOUNT_ID, NAME, EMAIL, PASSWORD, ACCOUNT_STATUS, IS_ADMIN)
+    VALUES
+    (1, 'Admin', 'admin@gmail.com', 'admin', 'Active', True),
+    (2, 'User', 'user@gmail.com', 'user', 'Active', False);
+    '''
+    cursor.execute(insert_sql)
+    print("Default values inserted into ACCOUNT_DIM successfully........")
+    
+    conn.commit()
+    conn.close()
+except:
+    print("Error creating Account table")
+
